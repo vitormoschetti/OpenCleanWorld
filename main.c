d6eec8244182d6ba80c6fcf82481880a3868bf28
Vitor Moschetti <vitormoschetti@terra.com.br>
2018-10-11 07:49:32 -0300

    Menu quase pronto

--- main.c
+++ main.c
@@ -1,75 +1,94 @@
-// Os arquivos de cabeçalho
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_image.h>
-
-// Para utilizarmos o fprintf
 #include <stdio.h>
+#include <stdbool.h>
 
-// Atributos da tela
-const int LARGURA_TELA = 1220;
-const int ALTURA_TELA = 720;
+const int LARGURA_TELA = 720;
+const int ALTURA_TELA = 432;
+
+ALLEGRO_DISPLAY *janela = NULL;
+ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
+ALLEGRO_BITMAP *fundo = NULL;
+ALLEGRO_FONT *fonte = NULL;
+
+bool inicializar();
 
 int main(void)
 {
-    ALLEGRO_DISPLAY *janela = NULL;
-    ALLEGRO_BITMAP *imagem = NULL;
-    ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
+    bool sair = false;
+    int tecla = 0;
 
-    if (!al_init())
+    if (!inicializar())
     {
-        fprintf(stderr, "Falha ao inicializar a Allegro.\n");
         return -1;
     }
 
-    if (!al_init_image_addon())
+    al_draw_bitmap(fundo, 0, 0, 0);
+
+    while (!sair)
     {
-        fprintf(stderr, "Falha ao inicializar add-on allegro_image.\n");
-        return -1;
-    }
-
-    janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
-    if (!janela)
-    {
-        fprintf(stderr, "Falha ao criar janela.\n");
-        return -1;
-    }
-
-    imagem = al_load_bitmap("res/menu.png");
-    if (!imagem)
-    {
-        fprintf(stderr, "Falha ao carregar o arquivo de imagem.\n");
-        al_destroy_display(janela);
-        return -1;
-    }
-
-    fila_eventos = al_create_event_queue();
-    if (!fila_eventos)
-    {
-        fprintf(stderr, "Falha ao criar fila de eventos.\n");
-        al_destroy_display(janela);
-        return -1;
-    }
-
-    al_register_event_source(fila_eventos, al_get_display_event_source(janela));
-
-    al_draw_bitmap(imagem, 0, 0, 0);
-
-    al_flip_display();
-
-    while (1)
-    {
-        ALLEGRO_EVENT evento;
-        ALLEGRO_TIMEOUT timeout;
-        al_init_timeout(&timeout, 0.05);
-
-        int tem_eventos = al_wait_for_event_until(fila_eventos, &evento, &timeout);
-
-        if (tem_eventos && evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+        while(!al_is_event_queue_empty(fila_eventos))
         {
-            break;
+            ALLEGRO_EVENT evento;
+            al_wait_for_event(fila_eventos, &evento);
+
+            if (evento.type == ALLEGRO_EVENT_KEY_DOWN)
+            {
+                switch(evento.keyboard.keycode)
+                {
+                case ALLEGRO_KEY_UP:
+                    tecla = 1;
+                    break;
+                case ALLEGRO_KEY_DOWN:
+                    tecla = 2;
+                    break;
+                case ALLEGRO_KEY_LEFT:
+                    tecla = 3;
+                    break;
+                case ALLEGRO_KEY_RIGHT:
+                    tecla = 4;
+                    break;
+                }
+            }
+            else if (evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+            {
+                sair = true;
+            }
         }
 
-        al_draw_bitmap(imagem, 0, 0, 0);
+        if (tecla)
+        {
+            al_draw_bitmap(fundo, 0, 0, 0);
+
+            switch (tecla)
+            {
+            case 1:
+                al_draw_text(fonte, al_map_rgb(255, 255, 255), LARGURA_TELA / 2,
+                        ALTURA_TELA / 2 - al_get_font_ascent(fonte) / 2,
+                        ALLEGRO_ALIGN_CENTRE, "Seta para cima");
+                break;
+            case 2:
+                al_draw_text(fonte, al_map_rgb(255, 255, 255), LARGURA_TELA / 2,
+                        ALTURA_TELA / 2 - al_get_font_ascent(fonte) / 2,
+                        ALLEGRO_ALIGN_CENTRE, "Seta para baixo");
+                break;
+            case 3:
+                al_draw_text(fonte, al_map_rgb(255, 255, 255), LARGURA_TELA / 2,
+                        ALTURA_TELA / 2 - al_get_font_ascent(fonte) / 2,
+                        ALLEGRO_ALIGN_CENTRE, "Seta esquerda");
+                break;
+            case 4:
+                al_draw_text(fonte, al_map_rgb(255, 255, 255), LARGURA_TELA / 2,
+                        ALTURA_TELA / 2 - al_get_font_ascent(fonte) / 2,
+                        ALLEGRO_ALIGN_CENTRE, "Seta direita");
+                break;
+            }
+
+            tecla = 0;
+        }
+
         al_flip_display();
     }
 
@@ -77,4 +96,72 @@ int main(void)
     al_destroy_event_queue(fila_eventos);
 
     return 0;
+}
+
+bool inicializar()
+{
+    if (!al_init())
+    {
+        fprintf(stderr, "Falha ao inicializar a Allegro.\n");
+        return false;
+    }
+
+    al_init_font_addon();
+
+    if (!al_init_ttf_addon())
+    {
+        fprintf(stderr, "Falha ao inicializar add-on allegro_ttf.\n");
+        return false;
+    }
+
+    if (!al_init_image_addon())
+    {
+        fprintf(stderr, "Falha ao inicializar add-on allegro_image.\n");
+        return false;
+    }
+
+    if (!al_install_keyboard())
+    {
+        fprintf(stderr, "Falha ao inicializar o teclado.\n");
+        return false;
+    }
+
+    janela = al_create_display(LARGURA_TELA, ALTURA_TELA);
+    if (!janela)
+    {
+        fprintf(stderr, "Falha ao criar janela.\n");
+        return false;
+    }
+
+    al_set_window_title(janela, "Utilizando o Teclado");
+
+    fonte = al_load_font("res/comic.ttf", 72, 0);
+    if (!fonte)
+    {
+        fprintf(stderr, "Falha ao carregar \"fonte comic.ttf\".\n");
+        al_destroy_display(janela);
+        return false;
+    }
+
+    fila_eventos = al_create_event_queue();
+    if (!fila_eventos)
+    {
+        fprintf(stderr, "Falha ao criar fila de eventos.\n");
+        al_destroy_display(janela);
+        return false;
+    }
+
+    fundo = al_load_bitmap("res/menu.png");
+    if (!fundo)
+    {
+        fprintf(stderr, "Falha ao carregar imagem de fundo.\n");
+        al_destroy_display(janela);
+        al_destroy_event_queue(fila_eventos);
+        return false;
+    }
+
+    al_register_event_source(fila_eventos, al_get_keyboard_event_source());
+    al_register_event_source(fila_eventos, al_get_display_event_source(janela));
+
+    return true;
 }
