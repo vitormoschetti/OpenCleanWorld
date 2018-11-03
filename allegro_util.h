f440b4d5f8f8c58a345a9203efb5f309b05070bd
Vitor Moschetti <vitormoschetti@terra.com.br>
2018-11-02 22:19:02 -0300

    Menu funcionando, configurando o jogo e alterando imagens e ações

--- allegro_util.h
+++
@@ -1,8 +0,0 @@
-#ifndef ALLEGRO_UTIL_H_INCLUDED
-#define ALLEGRO_UTIL_H_INCLUDED
-
-int inicializa_allegro(void);
-void finaliza_allegro(void);
-void error_msg(char *text);
-
-#endif // ALLEGRO_UTIL_H_INCLUDED
